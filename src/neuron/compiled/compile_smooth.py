38e1125593d2c68f6d754317fa9d85d65f2b8772
MarkRacsko <racskomark482@gmail.com>
2026-01-26 15:15:12 +0100

    compilation fixed

--- src/neuron/compiled/compile_smooth.py
+++ src/neuron/compiled/compile_smooth.py
@@ -1,13 +1,15 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
-from pathlib import Path
 import numpy as np
 
-extension = ["cy_smooth", ["cy_smooth.pyx"]]
-parent = Path(__file__).parent
+extension = Extension(
+    name="cy_smooth",
+    sources=["cy_smooth.pyx"],
+    include_dirs=[np.get_include()]
+)
 
 setup(
-    ext_modules=cythonize(str(parent) + "/cy_smooth.pyx"),
+    ext_modules=cythonize(extension),
     include_dirs=[np.get_include()],
     py_modules=[],
 )
