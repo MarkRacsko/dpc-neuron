dad43806724da1143a9737a7e5a8bc4cdd4320c0
MarkRacsko <racskomark482@gmail.com>
2026-01-27 13:33:08 +0100

    file renaming after AOT compilation step refactor

--- src/neuron/analysis/compiled/compile_smooth.py
+++ src/neuron/analysis/compiled/compile_smooth.py
@@ -1,6 +1,9 @@
+import os
+import numpy as np
+
 from setuptools import setup, Extension
 from Cython.Build import cythonize
-import numpy as np
+from pathlib import Path
 
 extension = Extension(
     name="cy_smooth",
@@ -13,3 +16,13 @@ setup(
     include_dirs=[np.get_include()],
     py_modules=[],
 )
+
+folder = Path(__file__).parent
+for file in folder.iterdir():
+    if file.is_file():
+        if file.match("cy_smooth.c"):
+            os.remove(file) # not entirely necessary, but might as well
+        if file.match("cy_smooth.cpython*.so"):
+            os.rename(file, folder / "cy_smooth.so")
+        elif file.match("cy_smooth.cpython*.pyd"):
+            os.rename(file, folder / "cy_smooth.pyd")
