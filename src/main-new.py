7b9b8fdea9e726e6c9619afcbeea78c687de74e2
Márk Racskó <racskomark482@gmail.com>
2026-07-27 14:45:17 +0200

    remove row button added

--- src/main-new.py
+++ src/main-new.py
@@ -13,23 +13,34 @@ def _(tabs):
 @app.cell
 def _(mo):
     get_rows, set_rows = mo.state([
-        {"name": "baseline", "range": (0, 100)}
+        {"name": "baseline", "range": [0, 60]}
     ])
+    # What this state is:
+    # A list of dictionaries, where each dict stores the following:
+    # name: the string entered into the text box
+    # range: the current settings of the range selector -- max value is frame_number.value
+    # range is a list not a tuple because that's what range_slider.value returns
 
     def add_row(_):
         current_rows = get_rows()
         set_rows(current_rows + [{"name": "New", "range": (0, 50)}])
 
-    add_btn = mo.ui.button(label="Add Row", on_click=add_row)
-    return add_btn, get_rows, set_rows
+    def remove_row(_):
+        current_rows = get_rows()
+        current_rows.pop() # remove last element
+        set_rows(current_rows)
+
+    add_btn = mo.ui.button(label="Add new row", on_click=add_row)
+    remove_btn = mo.ui.button(label="Remove last row", on_click=remove_row)
+    return add_btn, get_rows, remove_btn, set_rows
 
 
 @app.cell
-def _(add_btn, get_rows, mo, set_rows):
+def _(add_btn, get_rows, mo, remove_btn, set_rows):
     ui_container = mo.ui.array([
         mo.ui.dictionary({
             "name": mo.ui.text(value=row["name"]),
-            "range": mo.ui.range_slider(0, 200, value=row["range"])
+            "range": mo.ui.range_slider(0, 200, value=row["range"], full_width=True)
         })
         for row in get_rows()
     ], on_change=set_rows)
@@ -39,7 +50,7 @@ def _(add_btn, get_rows, mo, set_rows):
         for row in ui_container
     ])
 
-    treatment_stack = mo.vstack([rows_layout, add_btn])
+    treatment_stack = mo.vstack([rows_layout, mo.hstack([add_btn, remove_btn], justify="start")])
     return (treatment_stack,)
 
 
@@ -91,7 +102,7 @@ def _(
         mo.md(text="##Placeholder..."),
         metadata_file,
         mo.md(text="##Conditions"),
-        ratiometric,
+        mo.hstack([mo.md(text="Ratiometric dye:"), ratiometric], justify="start"),
         framerate,
         frame_number,
         group_1,
@@ -126,7 +137,7 @@ def _(mo):
     photo_corr = mo.ui.switch()
 
     metadata_file = mo.ui.file_browser(label="Measurement folder:", selection_mode="directory", multiple=False)
-    ratiometric = mo.ui.switch(label="Ratiometric dye")
+    ratiometric = mo.ui.switch()
     framerate = mo.ui.number(label="Framerate", start=1, stop=1000, value=60)
     frame_number = mo.ui.number(label="Number of frames", start=1, stop=1000000, step=1, value=300)
     group_1 = mo.ui.text(label="Group 1:")
@@ -156,15 +167,14 @@ def _(mo):
     mo.md(r"""
     # TODO
 
-    1. Implement reading config from file
-    2. remove_row button
-    3. Add folder selection to the metadata tab, build a blank metadata file from template, if no metadata file exists.
-    4. Setting the initial state by reading the contents of the metadata.toml file
-    5. Implement saving the file.
-    6. Figure out how to make the metadata file selector's initial path be the folder selected by target_folder
-    7. ~~Build the main panel with the 4 buttons~~, hook them up to the data processing backend
-    8. Add a progress bar to provide feedback on data analysis and file conversions.
-    9. Reconsider the program's architecture and general behavior. It may be better for repeated analysis with different settings to keep all input data in memory, instead of re-reading cached files. I don't remember exactly why I chose this design, and it may well be the correct one, but I will need to think about this more.
+    - Implement reading config from file
+    - Add folder selection to the metadata tab, build a blank metadata file from template, if no metadata file exists.
+    - Setting the initial state by reading the contents of the metadata.toml file
+    - Implement saving the file.
+    - Figure out how to make the metadata file selector's initial path be the folder selected by target_folder
+    - ~~Build the main panel with the 4 buttons~~, hook them up to the data processing backend
+    - Add a progress bar to provide feedback on data analysis and file conversions.
+    - Reconsider the program's architecture and general behavior. It may be better for repeated analysis with different settings to keep all input data in memory, instead of re-reading cached files. I don't remember exactly why I chose this design, and it may well be the correct one, but I will need to think about this more.
     """)
     return
 
